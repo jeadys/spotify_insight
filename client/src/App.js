e7fa851f4c8db8c1c9646d91f52e2297a6198e7f
jeadys <yassin.ao.work@gmail.com>
2022-06-25 15:59:38 +0200

    Setting up React router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import { useState, useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { accessToken, logout, getCurrentUserProfile } from "./spotify";
 import "./App.css";
 
-function App() {
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+const App = () => {
   const [token, setToken] = useState(null);
   const [profile, setProfile] = useState(null);
 
@@ -32,22 +43,34 @@ function App() {
           Log in to Spotify
         </a>
       ) : (
-        <>
-          <h1>Logged in!</h1>
-          <button onClick={logout}>Log out</button>
-          {profile && (
-            <div>
-              <h1>{profile.display_name}</h1>
-              <p>{profile.followers.total} Followers</p>
-              {profile.images.length && profile.images[0].url && (
-                <img src={profile.images[0].url} alt="Avatar" />
-              )}
-            </div>
-          )}
-        </>
+        <Routes>
+          <ScrollToTop />
+          <Route path="/top-artists" element={<h1>Top Artists</h1>} />
+          <Route path="/top-tracks" element={<h1>Top Tracks</h1>} />
+          <Route path="/playlists/:id" element={<h1>Playlist</h1>} />
+          <Route path="/playlists" element={<h1>Playlists</h1>} />
+          <Route
+            path="/"
+            element={
+              <>
+                <button onClick={logout}>Log Out</button>
+
+                {profile && (
+                  <div>
+                    <h1>{profile.display_name}</h1>
+                    <p>{profile.followers.total} Followers</p>
+                    {profile.images.length && profile.images[0].url && (
+                      <img src={profile.images[0].url} alt="Avatar" />
+                    )}
+                  </div>
+                )}
+              </>
+            }
+          ></Route>
+        </Routes>
       )}
     </div>
   );
-}
+};
 
 export default App;
