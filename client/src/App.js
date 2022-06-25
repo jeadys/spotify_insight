69a51d520ab360c70bc6ac29f6132fe8a2fccc0f
jeadys <yassin.ao.work@gmail.com>
2022-06-25 14:03:10 +0200

    Retrieve access and refresh token from url

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const accessToken = urlParams.get("access_token");
+    const refreshToken = urlParams.get("refresh_token");
+  }, []);
+
   return (
     <div className="App">
       <a
