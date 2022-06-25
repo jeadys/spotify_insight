f49964ffd5ab0f7e9c7f5184b23d1f523abdc2d2
jeadys <yassin.ao.work@gmail.com>
2022-06-25 14:02:13 +0200

    Remove React boilertemplate

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <a
+        className="App-link"
+        href="http://localhost:8888/login"
+        rel="noopener noreferrer"
+      >
+        Log in to Spotify
+      </a>
+    </div>
+  );
+}
+
+export default App;
