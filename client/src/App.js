17ac82572b038cd16c662c4e28a9a389a3e61bf2
jeadys <yassin.ao.work@gmail.com>
2022-06-25 14:22:42 +0200

    Refresh accesstoken when expired

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,16 @@ function App() {
     const urlParams = new URLSearchParams(queryString);
     const accessToken = urlParams.get("access_token");
     const refreshToken = urlParams.get("refresh_token");
+
+    console.log(accessToken);
+    console.log(refreshToken);
+
+    if (refreshToken) {
+      fetch(`/refresh_token?refresh_token=${refreshToken}`)
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.error(err));
+    }
   }, []);
 
   return (
