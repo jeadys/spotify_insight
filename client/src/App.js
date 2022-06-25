50c4662182036fbd26f21f47a6375e4267bdf388
jeadys <yassin.ao.work@gmail.com>
2022-06-25 15:28:40 +0200

    Get basic userprofile information from api endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
+import { useState, useEffect } from "react";
+import { accessToken, logout, getCurrentUserProfile } from "./spotify";
 import "./App.css";
-import { accessToken, logout } from "./spotify";
-import { useEffect, useState } from "react";
 
 function App() {
   const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
   useEffect(() => {
     setToken(accessToken);
+
+    const fetchData = async () => {
+      try {
+        const { data } = await getCurrentUserProfile();
+        setProfile(data);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
@@ -23,6 +35,15 @@ function App() {
         <>
           <h1>Logged in!</h1>
           <button onClick={logout}>Log out</button>
+          {profile && (
+            <div>
+              <h1>{profile.display_name}</h1>
+              <p>{profile.followers.total} Followers</p>
+              {profile.images.length && profile.images[0].url && (
+                <img src={profile.images[0].url} alt="Avatar" />
+              )}
+            </div>
+          )}
         </>
       )}
     </div>
