f7b218c853dd8b952803d27c07c2ebb7595441e6
jeadys <yassin.ao.work@gmail.com>
2022-06-27 19:48:08 +0200

    Tailwind configuration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '1600px',
+        '4xl': '1800px',
+        '5xl': '1920px',
+        '6xl': '2280px',
+        '7xl': '2560px'
+      },
+      maxWidth: {
+        '8xl': '90rem',
+      },
+      fontFamily: {
+        'maven': 'Maven Pro'
+      },
+      backgroundImage: (theme) => ({
+        login: "url('/public/images/login.jpg')",
+      }),
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    // ...
+  ],
 };
