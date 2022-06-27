0392f82ccbeb0e22ca9bce8c7babf684908a17c7
jeadys <yassin.ao.work@gmail.com>
2022-06-27 19:47:14 +0200

    Modify scope of authorization

--- index.js
+++ index.js
@@ -44,7 +44,8 @@ app.get("/login", (req, res) => {
   const state = generateRandomString(16);
   res.cookie(stateKey, state);
 
-  const scope = "user-read-private user-read-email";
+  const scope =
+    "user-read-private user-read-email user-top-read user-follow-read user-library-read";
 
   const queryParams = querystring.stringify({
     client_id: CLIENT_ID,
