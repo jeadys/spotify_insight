cc0dbae7d993911da0d0320afe2a485428bb5dbb
jeadys <yassin.ao.work@gmail.com>
2022-06-27 20:01:13 +0200

    Add login screen

---
+++ client/src/pages/Login.js
@@ -0,0 +1,13 @@
+export default function Login() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <a
+        className="p-5 text-white bg-green-600 hover:bg-green-700 transition ease-in-out rounded-full"
+        href="http://localhost:8888/login"
+        rel="noopener noreferrer"
+      >
+        Connect Spotify
+      </a>
+    </div>
+  );
+}
