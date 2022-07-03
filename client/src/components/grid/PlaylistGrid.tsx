b3f24f6d22985c51c17b82b422b0e32a80c33902
jeadys <yassin.ao.work@gmail.com>
2022-07-03 14:04:35 +0200

    Convert playlistgrid.js to typescript

--- client/src/components/grid/PlaylistGrid.tsx
+++ client/src/components/grid/PlaylistGrid.tsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
+import { IUsersPlaylists } from "../../common/interfaces/usersPlaylists";
 
-export default function PlaylistGrid({ playlists }) {
+export default function PlaylistGrid({ items }: IUsersPlaylists) {
   return (
     <>
-      {playlists && playlists.length ? (
+      {items && items.length ? (
         <>
           <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 5xl:grid-cols-6 6xl:grid-cols-6">
-            {playlists.map((playlist) => (
+            {items.map((playlist) => (
               <li
                 key={playlist.id}
                 className="col-span-1 flex flex-col text-center bg-slate-800 rounded-lg shadow hover:bg-slate-700 transition ease-in-out cursor-pointer"
