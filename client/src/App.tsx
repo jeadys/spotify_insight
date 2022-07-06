4866511a191c20ea0cb0051c369b681e4fad80e0
jeadys <yassin.ao.work@gmail.com>
2022-07-06 13:15:23 +0200

    Add react-query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,40 +9,47 @@ import {
   Album,
 } from "./pages";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { Content, ScrollTop, Search } from "./components";
 import TrackProvider from "./components/TrackContext";
 import { accessToken } from "./spotify";
 import "./App.css";
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <div className="App bg-gray-900 min-h-screen font-maven">
-      {!accessToken ? (
-        <Login />
-      ) : (
-        <div className="min-h-screen">
-          <Router>
-            <Content>
-              <ScrollTop />
-              <TrackProvider>
-                <Search />
-                <div className="space-y-10">
-                  <Routes>
-                    <Route path="/" element={<Profile />}></Route>
-                    <Route path="/top-artists" element={<TopArtists />} />
-                    <Route path="/top-tracks" element={<TopTracks />} />
-                    <Route path="/playlists" element={<Playlists />} />
-                    <Route path="/playlists/:id" element={<Playlist />} />
-                    <Route path="/artists/:id" element={<Artist />} />
-                    <Route path="/albums/:id" element={<Album />} />
-                  </Routes>
-                </div>
-              </TrackProvider>
-            </Content>
-          </Router>
-        </div>
-      )}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App bg-gray-900 min-h-screen font-maven">
+        {!accessToken ? (
+          <Login />
+        ) : (
+          <div className="min-h-screen">
+            <Router>
+              <Content>
+                <ScrollTop />
+                <TrackProvider>
+                  <Search />
+                  <div className="space-y-10">
+                    <Routes>
+                      <Route path="/" element={<Profile />}></Route>
+                      <Route path="/top-artists" element={<TopArtists />} />
+                      <Route path="/top-tracks" element={<TopTracks />} />
+                      <Route path="/playlists" element={<Playlists />} />
+                      <Route path="/playlists/:id" element={<Playlist />} />
+                      <Route path="/artists/:id" element={<Artist />} />
+                      <Route path="/albums/:id" element={<Album />} />
+                    </Routes>
+                  </div>
+                </TrackProvider>
+              </Content>
+            </Router>
+          </div>
+        )}
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
