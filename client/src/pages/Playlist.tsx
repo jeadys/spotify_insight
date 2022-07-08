619fd91bc793bf23de4c8219a8c1159b907cceba
jeadys <yassin.ao.work@gmail.com>
2022-07-08 17:28:51 +0200

    TEMP FIX: playlist loading

--- client/src/pages/Playlist.tsx
+++ client/src/pages/Playlist.tsx
@@ -6,58 +6,56 @@ import { useParams } from "react-router-dom";
 
 import axios from "axios";
 import { IPlaylist, Tracks } from "../common/interfaces/playlist";
+import { useQuery } from "react-query";
 
 export default function Playlist() {
   const { id } = useParams();
-  const [playlist, setPlaylist] = useState<IPlaylist>();
-  const [tracksData, setTracksData] = useState<Tracks>();
-  const [tracks, setTracks] = useState<Tracks["items"]>();
+  // const [tracksData, setTracksData] = useState<Tracks>();
+  // const [tracks, setTracks] = useState<Tracks["items"]>();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { data } = await getPlaylistById(id!);
-        setPlaylist(data);
-        setTracksData(data.tracks);
-      } catch (e) {
-        console.error(e);
-      }
-    };
+  const fetchPlaylist = async () => {
+    const playlist = await getPlaylistById(id!);
+    // setTracksData(playlist.data.tracks);
+    return playlist.data;
+  };
 
-    fetchData();
-  }, [id]);
+  const {
+    data: playlist,
+    isLoading: playlistIsLoading,
+    error: playlistError,
+  } = useQuery<IPlaylist>(["playlist", id], fetchPlaylist);
 
   // When tracksData updates, compile arrays of tracks and audioFeatures
-  useEffect(() => {
-    if (!tracksData) {
-      return;
-    }
+  // useEffect(() => {
+  //   if (!tracksData) {
+  //     return;
+  //   }
 
-    // When tracksData updates, check if there are more tracks to fetch
-    // then update the state variable
-    const fetchMoreData = async () => {
-      try {
-        if (tracksData.next) {
-          const { data } = await axios.get(tracksData.next);
-          setTracksData(data);
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    };
+  //   // When tracksData updates, check if there are more tracks to fetch
+  //   // then update the state variable
+  //   const fetchMoreData = async () => {
+  //     try {
+  //       if (tracksData.next) {
+  //         const { data } = await axios.get(tracksData.next);
+  //         setTracksData(data);
+  //       }
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //   };
 
-    setTracks((tracks) => [...(tracks ? tracks : []), ...tracksData.items]);
+  //   setTracks((tracks) => [...(tracks ? tracks : []), ...tracksData.items]);
 
-    fetchMoreData();
-  }, [tracksData]);
+  //   fetchMoreData();
+  // }, [tracksData]);
 
-  const tracksForTracklist: any = useMemo(() => {
-    // TODO CHANGE ANY DATATYPE
-    if (!tracks) {
-      return;
-    }
-    return tracks.map(({ track }) => track);
-  }, [tracks]);
+  // const tracksForTracklist: any = useMemo(() => {
+  //   // TODO CHANGE ANY DATATYPE
+  //   if (!tracks) {
+  //     return;
+  //   }
+  //   return tracks.map(({ track }) => track);
+  // }, [tracks]);
 
   return (
     <>
@@ -65,7 +63,9 @@ export default function Playlist() {
         <>
           <SectionWrapper title="Playlists" breadcrumb="true">
             <Header data={playlist} />
-            <TrackGrid items={tracksForTracklist} />
+            <TrackGrid
+              items={playlist.tracks.items.map(({ track }) => track)}
+            />
           </SectionWrapper>
         </>
       )}
