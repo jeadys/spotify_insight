c04bdf19496a25f40540b3f5ae0896b75877e439
jeadys <yassin.ao.work@gmail.com>
2022-08-02 21:47:44 +0200

    Permanent redirect to profile page from main path

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/profile",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
