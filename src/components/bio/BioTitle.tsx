bbe8dbf08adee9fb359bf7e6dc0cf60e8ebed81b
jeadys <yassin.ao.work@gmail.com>
2023-03-22 23:09:43 +0100

    Remove unused files

--- src/components/bio/BioTitle.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-type Props = {
-  title: string
-}
-
-export default function BioTitle({ title }: Props) {
-  return <span className="block font-semibold text-white md:text-lg">{title}</span>
-}
--- src/components/bio/BioValue.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-type Props = {
-  value: string
-}
-
-export default function BioValue({ value }: Props) {
-  return <span className="font-black text-blue-400 md:text-2xl">{value}</span>
-}
