6d297b633fcd0513532fe583d014f8363b00a8e0
jeadys <yassin.ao.work@gmail.com>
2023-03-22 22:13:38 +0100

    Change base layout of app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
-import '../styles/globals.css'
+import '@/styles/globals.css'
 
 import { Saira } from '@next/font/google'
 
-import Navigation from '@/components/core/Navigation'
 import NextAuthWrapper from '@/components/core/NextAuthWrapper'
 import ReactQueryWrapper from '@/components/core/ReactQueryWrapper'
 import ScrollToTop from '@/components/core/ScrollToTop'
+import Navigation from '@/components/layout/Navigation'
 import PlayedTrackProvider from '@/providers/PlayedTrackProvider'
 import UserProfileProvider from '@/providers/UserProfileProvider'
 import { getCurrentUsersProfile } from '@/server/api'
@@ -31,10 +31,10 @@ export default async function RootLayout({ children }: Props) {
             <UserProfileProvider userProfile={userProfile}>
               <PlayedTrackProvider>
                 <ScrollToTop />
-                <div className="min-h-screen bg-gray-900">
-                  <div className="mx-auto max-w-8xl p-5">
+                <div className="min-h-screen bg-gray-1300">
+                  <div className="mx-auto min-h-screen max-w-7xl py-5 px-6 sm:px-10">
                     <Navigation />
-                    <main>{children}</main>
+                    <main className="grid-col grid gap-10">{children}</main>
                   </div>
                 </div>
                 <div id="trackplayer" className="sticky bottom-0" />
