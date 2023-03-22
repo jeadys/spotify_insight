99016c15a1dc247938af16934b2b95d86b0b31d3
jeadys <yassin.ao.work@gmail.com>
2023-03-22 22:58:02 +0100

    Add label component and change folder directory of buttons

---
+++ src/components/ui/button/DiscoverButton.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import Link from 'next/link'
+
+type Props = {
+  titleMessage: string
+  buttonMessage: string
+}
+
+export default function DiscoverButton({ titleMessage, buttonMessage }: Props) {
+  return (
+    <div className="flex flex-col items-center gap-5 text-white">
+      <h3>{titleMessage}</h3>
+      <Link href={`/discover`} className="text-sm text-gray-300 underline">
+        {buttonMessage}
+      </Link>
+    </div>
+  )
+}
