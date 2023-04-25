df7efe12d924e63e2deefda011205da623d9a0a2
jeadys <yassin.ao.work@gmail.com>
2023-04-26 01:54:31 +0200

    Add api call for retrieving album tracks

--- src/server/api.ts
+++ src/server/api.ts
@@ -55,6 +55,18 @@ export const getAlbumById = async (albumId: string): Promise<SpotifyApi.SingleAl
 }
 
 /**
+ * Get an Album’s Tracks
+ *
+ * GET /v1/albums/{id}/tracks
+ * https://developer.spotify.com/web-api/get-albums-tracks/
+ * @param {string} albumId
+ * @param {number} limit Total amount of items to return
+ */
+export const getAlbumTracks = async (albumId: string, limit: number): Promise<SpotifyApi.AlbumTracksResponse> => {
+  return fetchWrapper({ url: `https://api.spotify.com/v1/albums/${albumId}/tracks?limit=${limit}`, method: 'GET' })
+}
+
+/**
  * Get a playlist
  *
  * GET /v1/playlists/{id}
