21cb9e44c5a5acf67b6812182666e8b9ca19a437
jeadys <yassin.ao.work@gmail.com>
2023-04-30 15:23:06 +0200

    Add playlist page

---
+++ src/components/header/PlaylistHeader.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import MetadataGrid from '@/components/analysis/MetadataGrid'
+import MetadataItem from '@/components/analysis/MetadataItem'
+import Header from '@/components/layout/Header'
+import { getPlaylistById } from '@/server/api'
+
+type Props = {
+  playlistId: string
+}
+
+export default async function AlbumHeader({ playlistId }: Props) {
+  const playlist = await getPlaylistById(playlistId)
+
+  return (
+    <Header>
+      <div className="flex flex-col items-center gap-4 sm:flex-row">
+        <Image
+          src={playlist.images?.[1]?.url || '/images/nocover.webp'}
+          alt={playlist.name}
+          width="0"
+          height="0"
+          sizes="100vw"
+          priority={true}
+          className="h-52 w-52 rounded-md object-cover sm:h-60 sm:w-60"
+        />
+
+        <div className="capitalize text-white">
+          <h2>{playlist.type}</h2>
+          <h1 className="text-3xl font-black sm:text-4xl">{playlist.name}</h1>
+          <Link href={playlist.owner.external_urls.spotify} className="text-xl text-gray-300 hover:underline">
+            {playlist.owner.display_name}
+          </Link>
+        </div>
+      </div>
+
+      <MetadataGrid>
+        <MetadataItem title="Tracks" value={playlist.tracks.total} />
+      </MetadataGrid>
+    </Header>
+  )
+}
---
+++ src/components/track/PlaylistTrackList.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import Image from 'next/image'
+
+import PlaybackHandle from '@/components/playback/PlaybackHandle'
+import TrackArtist from '@/components/track/TrackArtist'
+import TrackDuration from '@/components/track/TrackDuration'
+import TrackName from '@/components/track/TrackName'
+import { getPlaylistTracks } from '@/server/api'
+
+type Props = {
+  playlistId: string
+}
+
+export default async function PlaylistTrackList({ playlistId }: Props) {
+  const playlistTracks = await getPlaylistTracks(playlistId, 50)
+  if (!playlistTracks?.items?.length) return <span className="text-white">No tracks found</span>
+
+  const trackQueue = playlistTracks.items.map(({ track }) => track.uri)
+
+  return (
+    <ul className="w-full">
+      {playlistTracks.items.map(({ track }) => (
+        <li key={track.id} className="group flex items-center gap-5 p-2 hover:bg-gray-1200">
+          <div className="relative flex flex-shrink-0 items-center justify-center">
+            <Image
+              src={track.album?.images?.[2]?.url || '/images/nocover.webp'}
+              alt={track.name}
+              width="0"
+              height="0"
+              sizes="100vw"
+              className="h-10 w-10 rounded-md object-cover group-hover:blur-xs"
+            />
+
+            <PlaybackHandle uri={track.uri} queue={trackQueue} />
+          </div>
+
+          <span className="flex-grow">
+            <TrackName trackId={track.id} trackName={track.name} />
+            <TrackArtist artistId={track.artists[0].id} artistName={track.artists[0].name} />
+          </span>
+
+          <TrackDuration trackDuration={track.duration_ms} />
+        </li>
+      ))}
+    </ul>
+  )
+}
