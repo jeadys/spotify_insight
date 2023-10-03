d9d863b4b1a3512363ad3efa5478f60852b74619
Jeadys <yassin.ao.work@gmail.com>
2023-10-03 23:01:03 +0200

    Add footer and change navigation

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,22 @@
+import { HeartIcon } from '@heroicons/react/solid'
+
+export default function Footer() {
+  return (
+    <>
+      <hr className="my-10 h-px border-0 bg-gray-900" />
+      <div className="my-5 flex flex-col justify-between gap-5 text-sm text-white sm:flex-row">
+        <div className="flex items-center">
+          <span className="mr-2">
+            Made with <HeartIcon className="inline h-6 w-6 text-red-600" /> in The Netherlands
+          </span>
+        </div>
+
+        <ul className="flex gap-5">
+          <li className="my-2">© 2023</li>
+          <li className="my-2">Privacy</li>
+          <li className="my-2">Terms</li>
+        </ul>
+      </div>
+    </>
+  )
+}
--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -3,7 +3,9 @@
 import type { ReactElement } from 'react'
 import { useEffect, useState } from 'react'
 
+import { AdjustmentsIcon, LogoutIcon, SearchIcon, UserIcon, BookmarkIcon } from '@heroicons/react/outline'
 import { signOut } from 'next-auth/react'
+import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
@@ -18,18 +20,22 @@ export default function Navigation() {
     {
       title: 'Search',
       link: '/search',
+      icon: <SearchIcon className="mr-2 h-6 w-6 text-white" />,
     },
     {
       title: 'Profile',
       link: '/profile',
+      icon: <UserIcon className="mr-2 h-6 w-6 text-white" />,
     },
-    {
-      title: 'Library',
-      link: '/library',
-    },
+    // {
+    //   title: 'Library',
+    //   link: '/library',
+    //   icon: <BookmarkIcon className="mr-2 h-6 w-6 text-white" />,
+    // },
     {
       title: 'Generator',
       link: '/generator',
+      icon: <AdjustmentsIcon className="mr-2 h-6 w-6 text-white" />,
     },
   ]
 
@@ -43,10 +49,15 @@ export default function Navigation() {
   return (
     <nav className="relative text-sm uppercase">
       <ul className="flex w-full flex-row justify-end gap-5 py-5 text-white">
-        <div className="mr-auto">LOGO</div>
+        <div className="mr-auto flex items-center">
+          <span className="text-xs">Powered By</span>
+          <Image src="/icons/spotify.svg" alt="Spotify" width="0" height="0" sizes="100vw" className="h-6 w-6 rounded-md fill-green-300 mx-2" />
+          <span className="text-xs">API</span>
+        </div>
         {navigationItems.map((navigationItem) => (
           <li key={navigationItem.title} className={'hidden rounded-md hover:bg-gray-1100 lg:block'}>
-            <Link className="block p-2" href={navigationItem.link}>
+            <Link className="flex items-center p-2" href={navigationItem.link}>
+              {navigationItem.icon}
               {navigationItem.title}
             </Link>
           </li>
@@ -71,7 +82,10 @@ export default function Navigation() {
         </button>
 
         <button className="ml-auto hidden lg:block" onClick={() => signOut()}>
-          Sign out
+          <span className="flex items-center rounded-md p-2 hover:bg-gray-1100">
+            <LogoutIcon className="mr-2 h-6 w-6 text-white" />
+            <span className="uppercase">Sign out</span>
+          </span>
         </button>
       </ul>
 
@@ -80,14 +94,19 @@ export default function Navigation() {
         <ul className="absolute right-0 z-30 w-1/2 max-w-max rounded-md bg-gray-1200 lg:hidden">
           {navigationItems.map((navigationItem) => (
             <li key={navigationItem.title}>
-              <Link href={navigationItem.link} className="block py-2 px-4 text-white hover:rounded-md hover:bg-gray-1100">
+              <Link href={navigationItem.link} className="flex items-center py-2 px-4 text-white hover:bg-gray-1100">
+                {navigationItem.icon}
                 {navigationItem.title}
               </Link>
             </li>
           ))}
-          {/* <button className="block py-2 px-4 text-white hover:bg-gray-1100" onClick={() => signOut()}>
-            Sign out
-          </button> */}
+          <hr className="my-2" />
+          <button className="block w-full py-2 px-4 text-left uppercase text-white hover:bg-gray-1100" onClick={() => signOut()}>
+            <span className="flex items-center rounded-md hover:bg-gray-1100">
+              <LogoutIcon className="mr-2 h-6 w-6 text-white" />
+              <span className="uppercase">Sign out</span>
+            </span>
+          </button>
         </ul>
       )}
     </nav>
