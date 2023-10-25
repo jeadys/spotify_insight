62d71d23fe14778921afcb7a3ceeb0d57b8321da
Jeadys <yassin.ao.work@gmail.com>
2023-10-25 22:11:59 +0200

    Get timeRange from url instead of state

--- src/components/layout/Section.tsx
+++ src/components/layout/Section.tsx
@@ -1,23 +1,21 @@
 'use client'
 
-import { useProfileFilterStore } from '@/store/useProfileFilter'
+import { TimeRangeDescription } from '../filter/TimeRangeDescription'
 
 type Props = {
   title: string
   description?: string
-  showTerm?: boolean
+  timeRange?: string
   children: React.ReactNode
 }
 
-export const Section = ({ title, description, showTerm, children }: Props) => {
-  const termDescription = useProfileFilterStore((state) => state.description)
-
+export const Section = ({ title, description, timeRange, children }: Props) => {
   return (
     <section>
       <header className="sticky top-0 z-10 bg-gray-1300 py-2 capitalize text-white sm:py-5">
         <h2 className="text-2xl font-black sm:text-3xl">{title}</h2>
         <h3 className="line-clamp-1 break-all text-gray-300">
-          {description} {showTerm && termDescription}
+          {description} {timeRange && <TimeRangeDescription timeRange={timeRange} />}
         </h3>
       </header>
       {children}
