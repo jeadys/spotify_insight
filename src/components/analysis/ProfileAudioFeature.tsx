2372a0b20d81ab9637bb35194f8b1fbb1215bf58
Jeadys <yassin.ao.work@gmail.com>
2024-04-24 22:44:18 +0200

    Update import for track api calls

--- src/components/analysis/ProfileAudioFeature.tsx
+++ src/components/analysis/ProfileAudioFeature.tsx
@@ -1,7 +1,7 @@
 import { ProgressBarGrid } from '@/components/analysis/ProgressBarGrid'
 import { ProgressBarItem } from '@/components/analysis/ProgressBarItem'
 import { TrackAudioFeatureScatter } from '@/components/analysis/TrackAudioFeatureScatter'
-import { getAudioFeaturesForMultipleTracks, getTopTracks } from '@/server/api'
+import { getAudioFeaturesForMultipleTracks } from '@/server/api/track'
 import { calculateaverageAudioFeature } from '@/utils/calculateAverageAudioFeature'
 import { mergeTracksWithAudioFeatures } from '@/utils/mergeTracksWithAudioFeatures'
 
